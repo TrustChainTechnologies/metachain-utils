1d87bec6f58e513fc19d73eb79dabae5519ea524
root <root@v22017054649549681.supersrv.de>
2017-09-16 13:47:06 +0200

    Fixed path creation

--- deploy/tctscript.sh
+++ deploy/tctscript.sh
@@ -14,7 +14,7 @@ exit 1
 
 #Check if the paths are vaild
 [[ ! -e $1 ]] && echo "Not a vaild input $1" && exit 1
-[[ -d $2 ]] || echo "No such directory $2 creating..."&& mkdir -p "$2"
+[[ -d $2 ]] || echo "No such directory $2 creating..."&& mkdir -p "$2" && mkdir -p "$2/deps"
 
 #Get the library dependencies
 echo "Collecting the shared library dependencies for $1..."
@@ -27,19 +27,17 @@ echo "Copying the dependencies to $2"
 for dep in $deps
 do
     echo "Copying $dep to $2"
-    cp "$dep" "$2"
+    cp "$dep" "$2/deps"
 done
 echo "Done!"
 #
 #
-mkdir ~/tct_temp
-mkdir ~/tct_temp/dependencies
-cp tctnoded ~/tct_temp
-cp node.ini ~/tct_temp
-cp bans.dat ~/tct_temp
-cp peers.dat ~/tct_temp
-cd ~/tct_temp
+cp tctnoded "$2"
+cp node.ini "$2"
+cp bans.dat "$2"
+cp peers.dat "$2"
+cd "$2"
 zip -r  tct.zip *
 cd ~
-yes "" | ./sha3 --test-file=/home/mpool/tct_temp/tct.tar.gz >  ~/tct_temp/check.sum
-echo "done"
+yes "" | ./sha3 --test-file="$2/tct.zip" >  "$2/check.sum"
+echo "Done!"
